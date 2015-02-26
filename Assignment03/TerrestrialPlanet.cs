4a289dcf821b210ac286ec5c66a9e46d5f1e4e07
mohamadaljaf <mohamadaljaf@gmail.com>
2015-02-26 02:43:53 -0500

    Completed TerrestrialPlanet class

---
+++ Assignment03/TerrestrialPlanet.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment03
+{
+    class TerrestrialPlanet : Planet
+    {
+        // INSTANCE VARIABLES ******************************************
+        private bool _oxygen;
+
+        // CONSTRUCTOR(S) ******************************************
+        public TerrestrialPlanet(string name, double diameter, double mass, bool oxygen) : base(name, diameter, mass)
+        {
+            this._oxygen = oxygen;
+        }
+
+        // PUBLIC METHODS ******************************************
+        // returns true if the moon count is greater than zero
+        public bool HasMoons()
+        {
+            if (this.MoonCount == 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        // returns true if the oxygen variable is set to true
+        public bool Habitable()
+        {
+            return this._oxygen;
+        }
+    }
+}
