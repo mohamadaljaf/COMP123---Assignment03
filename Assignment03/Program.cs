e0ebfd1400bcdb8897a74881df5f3e6b38f8d92e
mohamadaljaf <mohamadaljaf@gmail.com>
2015-02-27 19:56:06 -0500

    Finishing touches

--- Assignment03/Program.cs
+++ Assignment03/Program.cs
@@ -14,6 +14,7 @@
 ///                         2015-02-26: Completed GiantPlanet class
 ///                         2015-02-26: Completed TerrestrialPlanet class
 ///                         2015-02-27: Added 3 Interfaces, IHabitable, IHasRings, IHasMoons
+///                         2015-02-27: Finishing touches
 ///------------------------------------------------------------------------------------------
 using System;
 using System.Collections.Generic;
@@ -27,12 +28,48 @@ namespace Assignment03
     {
         static void Main(string[] args)
         {
-            GiantPlanet giantPlanet = new GiantPlanet("Jupiter", 139822, 317, "Gas");
+            GiantPlanet giantPlanet = new GiantPlanet("Saturn", 116464, 95.16, "Gas");
             TerrestrialPlanet terrestrialPlanet = new TerrestrialPlanet("Mars", 6779, 0.107, false);
 
             Console.WriteLine(giantPlanet.ToString());
-            Console.WriteLine(terrestrialPlanet.ToString());
+            Console.WriteLine(@"                                                                      ..;===+.
+                                                                  .:=iiiiii=+=
+                                                               .=i))=;::+)i=+,
+                                                            ,=i);)I)))I):=i=;
+                                                         .=i==))))ii)))I:i++
+                                                       +)+))iiiiiiii))I=i+:'
+                                  .,:;;++++++;:,.       )iii+:::;iii))+i='
+                               .:;++=iiiiiiiiii=++;.    =::,,,:::=i));=+'
+                             ,;+==ii)))))))))))ii==+;,      ,,,:=i))+=:
+                           ,;+=ii))))))IIIIII))))ii===;.    ,,:=i)=i+
+                          ;+=ii)))IIIIITIIIIII))))iiii=+,   ,:=));=,
+                        ,+=i))IIIIIITTTTTITIIIIII)))I)i=+,,:+i)=i+
+                       ,+i))IIIIIITTTTTTTTTTTTI))IIII))i=::i))i='
+                      ,=i))IIIIITLLTTTTTTTTTTIITTTTIII)+;+i)+i`
+                      =i))IIITTLTLTTTTTTTTTIITTLLTTTII+:i)ii:'
+                     +i))IITTTLLLTTTTTTTTTTTTLLLTTTT+:i)))=,
+                     =))ITTTTTTTTTTTLTTTTTTLLLLLLTi:=)IIiii;
+                    .i)IIITTTTTTTTLTTTITLLLLLLLT);=)I)))))i;
+                    :))IIITTTTTLTTTTTTLLHLLLLL);=)II)IIIIi=:
+                    :i)IIITTTTTTTTTLLLHLLHLL)+=)II)ITTTI)i=
+                    .i)IIITTTTITTLLLHHLLLL);=)II)ITTTTII)i+
+                    =i)IIIIIITTLLLLLLHLL=:i)II)TTTTTTIII)i'
+                  +i)i)))IITTLLLLLLLLT=:i)II)TTTTLTTIII)i;
+                +ii)i:)IITTLLTLLLLT=;+i)I)ITTTTLTTTII))i;
+               =;)i=:,=)ITTTTLTTI=:i))I)TTTLLLTTTTTII)i;
+             +i)ii::,  +)IIITI+:+i)I))TTTTLLTTTTTII))=,
+           :=;)i=:,,    ,i++::i))I)ITTTTTTTTTTIIII)=+'
+         .+ii)i=::,,   ,,::=i)))iIITTTTTTTTIIIII)=+
+        ,==)ii=;:,,,,:::=ii)i)iIIIITIIITIIII))i+:'
+       +=:))i==;:::;=iii)+)=  `:i)))IIIII)ii+'
+     .+=:))iiiiiiii)))+ii;
+    .+=;))iiiiii)));ii+
+   .+=i:)))))))=+ii+
+  .;==i+::::=)i=;
+  ,+==iiiiii+,
+  `+=+++;`");
 
+            Console.WriteLine(terrestrialPlanet.ToString());
             waitForAnyKey();
         }
 
@@ -41,7 +78,8 @@ namespace Assignment03
         {
             Console.Write("\n");
             Console.WriteLine("------------------------------");
-            Console.WriteLine("Please hit any key to exit....");
+            Console.WriteLine("Please hit any key to exit...");
+            
             Console.ReadKey();
         }
     }
